71ad525da78d45f95d6aceb8b2d1c42441d0364c
Young Fizz <fizzler@black.com>
2025-08-23 23:38:00 +0100

    Removed unused link export & unescaped quotes in footnote.

--- components/footnote.tsx
+++ components/footnote.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link';
-
 export function Footnote() {
   return (
     <div className="text-xs text-zinc-400 leading-5 hidden sm:block">
-      "Please do not drain my openrouter key" - @YoungFizz
+      &ldquo;Please do not drain my openrouter key&rdquo; - @YoungFizz
     </div>
   );
 }
