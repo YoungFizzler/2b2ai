ec4c5144c70a90a878a02d1d8b46bf5b7801a34a
Young Fizz <fizzler@black.com>
2025-08-23 23:45:30 +0100

    Updates metadata images, added href to portfolio & removed deploy-button component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body>
+      <body>  
         <div className="fixed right-0 left-0 w-full top-0 bg-white dark:bg-zinc-950">
           <div className="flex justify-between items-center p-4">
             <div className="flex flex-row items-center gap-2 shrink-0 ">
@@ -30,7 +30,7 @@ export default function RootLayout({
                   className="text-zinc-800 dark:text-zinc-100 -translate-y-[.5px]"
                   rel="noopener"
                   target="_blank"
-                  href="https://vercel.com/"
+                  href="https://youngfizz.co.uk"
                 >
                 <img
                           src="https://cdn.discordapp.com/avatars/1229934153454325843/e499ae24601d1fcd6883b05ebc25d535.webp?size=80"
